aed8cd87add98581247f757a3ae547d1f8194748
Gitoasty <65080822+Gitoasty@users.noreply.github.com>
2025-04-14 20:49:31 +0200

    first commit

---
+++ src/main/java/pwa/project/one_piece/OnePieceApplication.java
@@ -0,0 +1,13 @@
+package pwa.project.one_piece;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnePieceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnePieceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pwa/project/one_piece/OnePieceApplicationTests.java
@@ -0,0 +1,13 @@
+package pwa.project.one_piece;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnePieceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
