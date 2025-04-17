f3390233bc6d7d9ce0b8f6eabe405fab4eaf8b15
Gitoasty <matej.miklin11@gmail.com>
2025-04-17 09:19:27 +0200

    navigation from admin-main.html works, filtering by fruit possession works too

--- src/main/java/pwa/project/one_piece/views/AdminCharacterViewController.java
+++ src/main/java/pwa/project/one_piece/views/AdminCharacterViewController.java
@@ -17,6 +17,24 @@ public class AdminCharacterViewController {
     @GetMapping
     public String showCharactersPage(Model model) {
         model.addAttribute("characterList", characterService.getAllCharacters());
-        return "characters";
+        model.addAttribute("fruitFilter", false);
+        model.addAttribute("noFruitFilter", false);
+        return "admin-characters";
+    }
+
+    @GetMapping("/with-fruits")
+    public String showFruitUsers(Model model) {
+        model.addAttribute("characterList", characterService.findWithFruit());
+        model.addAttribute("fruitFilter", true);
+        model.addAttribute("noFruitFilter", false);
+        return "admin-characters";
+    }
+
+    @GetMapping("/without-fruits")
+    public String showNonFruitUsers(Model model) {
+        model.addAttribute("characterList", characterService.findWithoutFruit());
+        model.addAttribute("fruitFilter", false);
+        model.addAttribute("noFruitFilter", true);
+        return "admin-characters";
     }
 }
